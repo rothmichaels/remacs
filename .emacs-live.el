94c6b3fa30ebed918e49df7806b21290828f8c1d
Roth Michaels <roth@rothmichaels.us>
2013-03-31 17:39:05 -0400

    Pack updates.

--- .emacs-live.el
+++ .emacs-live.el
@@ -1,12 +1,13 @@
-(live-use-packs '(dev/foundation-pack
-                live/colour-pack
-                live/clojure-pack
-                live/lang-pack
-                live/power-pack
-		live/git-pack))
+(live-use-packs '(live/foundation-pack
+                  dev/colour-pack
+                  live/clojure-pack
+                  live/lang-pack
+                  live/power-pack
+                  live/git-pack))
 
 (live-add-packs '(~/.emacs-live-packs.d/roth-pack
-                  ~/.emacs-live-packs.d/mithrandir-theme))
+                  ~/.emacs-live-packs.d/mithrandir-theme
+                  ~/.emacs-live-packs.d/drupal-pack))
 
 (server-start)
 
