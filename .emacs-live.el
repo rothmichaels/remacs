f51a17ed3055c7a3a7dd9e857306636436e2b7d1
Roth Michaels <roth@rothmichaels.us>
2014-01-16 16:37:41 -0500

    Magit debugging.

--- .emacs-live.el
+++ .emacs-live.el
@@ -2,11 +2,12 @@
                   dev/colour-pack
                   live/clojure-pack
                   live/lang-pack
-                  live/power-pack
-                  live/git-pack))
+                  dev/power-pack
+       	          dev/git-pack))
 
 (live-add-packs '(~/.emacs-live-packs.d/roth-pack
                   ~/.emacs-live-packs.d/mithrandir-theme
                   ~/.emacs-live-packs.d/drupal-pack))
 
 (live-set-custom-file "~/git/remacs/custom-configuration.el" )
+
