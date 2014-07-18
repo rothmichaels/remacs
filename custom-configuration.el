c986d2723af0c6c4135b907349b8585ccdbc638a
Roth Michaels <roth@rothmichaels.us>
2014-07-17 23:10:22 -0400

    little config change

--- custom-configuration.el
+++ custom-configuration.el
@@ -4,10 +4,12 @@
 
 (live-set-default-darwin-font "Courier-14")
 
-(set-frame-position (selected-frame) 0 0)
-(set-frame-height (selected-frame) 66)
-(set-frame-width (selected-frame) 208)
-(split-window-right)
+;; (set-frame-position (selected-frame) 0 0)
+;; (set-frame-height (selected-frame) 66)
+;; (set-frame-width (selected-frame) 208)
+;; (split-window-right)
+
+(add-hook 'text-mode-hook 'turn-on-auto-fill)
 
 (server-start)
 
