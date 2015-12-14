f61a492a2c82f62cc942c52aa2d819ff6e27bc5a
Roth Michaels <roth@rothmichaels.us>
2015-12-14 14:31:27 -0500

    Don't load swift-pack

--- .emacs-live.el
+++ .emacs-live.el
@@ -9,8 +9,7 @@
 (live-add-packs '(~/.emacs-live-packs.d/roth-pack
                   ~/.emacs-live-packs.d/mithrandir-theme
 		  ~/.emacs-live-packs.d/nemacs/packs/buffer-move
-		  ~/.emacs-live-packs.d/scala-pack
-                  ~/.emacs-live-packs.d/swift-pack))
+		  ~/.emacs-live-packs.d/scala-pack))
 
 (live-set-custom-file "~/lib/remacs/custom-configuration.el" )
 
